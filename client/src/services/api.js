a519fcab904c31934c9abff47e3baf5e24c0956f
khizer-25 <khizerbaig172@gmail.com>
2026-07-13 22:39:19 +0530

    Connect frontend to deployed backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://billing-system-u0n0.onrender.com",
 });
 
 export default API;
